66470f195eaa14e9f026172b42ff498cf6b6eca1
whentea <wentyyuniarti@gmail.com>
2019-04-26 19:40:10 +0700

    LL

---
+++ LinkedList_Up1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+struct simpul
+{
+    int data;
+    simpul* lanjut;
+};
+
+void inisialisasi
+{
+    kepala = NULL;
+    ekor = NULL;
+}
+
+void input(int dt)
+{
+cout<<"under construction";
+}
+
+void hapus()
+{
+cout<<"under construction";
+}
+
+void cetak()
+{
+    cout<<"under construction";
+}
+
+void menu()
+{
+    cout<<"under construction";
+}
+
+int main()
+{
+    inisialisasi();
+    menu();
+    return EXIT_SUCCESS;
+}
